d96d2276be73131f984c75453d7a3b7d5867e929
Felix Zapata <fzberlinches@gmail.com>
2016-10-27 09:45:34 +0200

    refactor(axe.js): change code style

--- tasks/axe.js
+++ tasks/axe.js
@@ -1,34 +1,40 @@
 'use strict';
-var gulp = require('gulp');
-var axeWebDriver = require('gulp-axe-webdriver');
-var util = require('gulp-util');
-var existsUrlParam = process.argv.indexOf('-url') !== -1;
-var url = process.argv[4];
-var urlsToRead = [];
 
-var validateUrl = function (userInput) {
+const gulp = require('gulp');
+const axeWebDriver = require('gulp-axe-webdriver');
+const util = require('gulp-util');
+
+let existsUrlParam = process.argv.indexOf('-url') !== -1;
+let url = process.argv[4];
+
+let urlsToRead = [];
+
+let validateUrl = function (userInput) {
 	if (userInput === 'localhost') {
 		return true;
 	} else {
-		var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
-		return regexp.test(userInput);
+		return /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(userInput);
 	}
 };
 
-var returnJSONResults = function () {
-	var XMLPath = require('path').join(__dirname, 'a11y-theme-unit-test-data.xml');
-	var fs = require('fs');
-	var xml2js = require('xml2js');
-	var urls;
-	var json;
+let returnJSONResults = function () {
+
+	const XMLPath = require('path').join(__dirname, 'a11y-theme-unit-test-data.xml');
+	const fs = require('fs');
+	const xml2js = require('xml2js');
+	let urls;
+	let json;
+
 	try {
-		var fileData = fs.readFileSync(XMLPath, 'ascii');
-		var parser = new xml2js.Parser();
-		parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
+
+		let fileData = fs.readFileSync(XMLPath, 'ascii');
+		let parser = new xml2js.Parser();
+
+		parser.parseString(fileData.substring(0, fileData.length), (err, result) => {
 			json = JSON.stringify(result);
-			urls = result.rss.channel[0].item.filter(function (item) {
+			urls = result.rss.channel[0].item.filter((item) => {
 				return item['wp:post_type'][0] === 'page' || item['wp:post_type'][0] === 'post';
-			}).map(function (item) {
+			}).map((item) => {
 				return item.link[0].replace('http://wpthemetestdata.wordpress.com/', url);
 			});
 		});
@@ -39,11 +45,11 @@ var returnJSONResults = function () {
 
 };
 
-function readXML() {
+let readXML = function () {
 	return returnJSONResults();
 }
 
-gulp.task('axe', function (done) {
+gulp.task('axe', (done) => {
 	if (existsUrlParam && validateUrl(url)) {
 		var options = {};
 		if(urlsToRead.length === 0) {
